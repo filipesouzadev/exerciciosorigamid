6f5a826ffabd413e8b7d9e97e29074bb4eee0903
filipesouzadev <52901592+filipesouzadev@users.noreply.github.com>
2020-08-21 10:22:30 -0300

    Adicionando Exercícios

---
+++ foreach-e-arrowfunction/script-aula.js
@@ -0,0 +1,39 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linksInternos = document.querySelector('[href^="#"]');
+console.log(linksInternos);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+
+
+primeiraUl.classList.add('grid-section')
+
+console.log(gridSectionHTML);
+console.log(gridSectionNode);
+
+
+gridSectionNode.forEach(function(item, index){
+    console.log(item);
+});
+
+//Transforma em Array
+const arrayGrid = Array.from(gridSectionHTML);
+
+arrayGrid.forEach(function(item, index){
+    console.log(item);
+})
